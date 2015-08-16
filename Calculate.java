2e22d5c92a024ec12b24120c7b322f3586e83a60
jsmast <jsmast@ya.ru>
2015-08-16 21:39:28 +0300

    Add minus operator

--- Calculate.java
+++ Calculate.java
@@ -4,6 +4,8 @@ public class Calculate {
 		int first = Integer.valueOf(arg[0]);
 		int second = Integer.valueOf(arg[1]);
 		int sum = first + second;
+		int minus = first - second;
 		System.out.println("Sum " + sum);
+		System.out.println("Minus " + minus);
 	}
 }
